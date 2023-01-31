0c01367dcbaee7520fae5244b095b4c304e16ca3
sahyunjin <tkguswls1106@naver.com>
2023-01-31 13:56:41 +0900

    Memo 관련 다시 List에서 Set으로 변경.

--- src/main/java/com/shj/onlinememospringproject/domain/dto/Memo/MemoResponseDto.java
+++ src/main/java/com/shj/onlinememospringproject/domain/dto/Memo/MemoResponseDto.java
@@ -5,7 +5,7 @@ import com.shj.onlinememospringproject.domain.jpo.UserAndMemo;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.Set;
 
 @Getter
 @NoArgsConstructor
@@ -17,7 +17,7 @@ public class MemoResponseDto {  // 요청받아 가져오는 DTO. 예를들어 C
     private LocalDateTime modifiedDate;
     private Integer isStar;
 
-    private List<UserAndMemo> userAndMemos;
+    private Set<UserAndMemo> userAndMemos;
 
     // repository를 통해 조회한 entity를 dto로 변환 용도
     public MemoResponseDto(Memo entity) {
--- src/main/java/com/shj/onlinememospringproject/domain/jpo/Memo.java
+++ src/main/java/com/shj/onlinememospringproject/domain/jpo/Memo.java
@@ -4,8 +4,8 @@ import lombok.*;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @NoArgsConstructor
@@ -36,7 +36,7 @@ public class Memo implements Serializable {
     // 참고로 이건 db에 안나타남.
     @Builder.Default
     @OneToMany(mappedBy = "memo")
-    private List<UserAndMemo> userAndMemos = new ArrayList<>();
+    private Set<UserAndMemo> userAndMemos = new HashSet<>();
 
 
     @Builder
